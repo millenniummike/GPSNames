ed8a86755066a1790c67d6b729fd3787355d9f74
Mike Griffin <mike_griffin@hotmail.co.uk>
2012-07-11 13:23:30 +0100

    new from sencha build

--- sencha.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" type="text/css" href="sencha-touch.css" />
-<link rel="stylesheet" type="text/css" href="resources/css/app.css" />
-<script type="text/javascript" src="sencha-touch.js"></script>
-<script type="text/javascript" src="app.js"></script>
-
-
-  
